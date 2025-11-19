2d5f67131580dd39def54c8a9261d5657fada175
kig7474 <89111461+kig7474@users.noreply.github.com>
2025-11-18 21:26:08 -0300

    Add files via upload

---
+++ função imprime texto.c
@@ -0,0 +1,28 @@
+static void imprimirTexto(void)
+{
+  if (!g_conectada) {  //Verifica se a impressora está conectada
+        printf("ERRO: Não é possível imprimir. A impressora não está conectada.\n");
+        return;
+    }
+
+    char texto[512];
+
+    printf("\nDigite o texto que deseja imprimir:\n> "); //Lê o texto digitado pelo usuário
+    flush_entrada();
+    fgets(texto, sizeof(texto), stdin);
+
+    // Remove quebra de linha automática
+    texto[strcspn(texto, "\n")] = '\0';
+
+    int ret = ImpressaoTexto(texto, 0, 0, 0); //Envia o texto diretamente para a impressora com ImpressaoTexto()
+    if (ret != 0) {
+        printf("Erro ao imprimir o texto! Código retornado: %d\n", ret);
+        return;
+    }
+
+    AvancaPapel(10);  // empurra o papel para frente
+    Corte(0);          // 0 = corte total
+
+    printf("\nTexto impresso com sucesso!\n");
+}
+
